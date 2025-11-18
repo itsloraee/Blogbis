4b061307ed21a4a078affb0935ab96d92de63c53
itsloraee <laurxee@gmail.com>
2025-11-18 10:01:20 -0400

    finalisation model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     protected $fillable = ['user_id' , 'content'];
+
+    public function user() : BelongsTo
+    {
+        // Define the relationship with the User model
+        return $this->belongsTo(User::class);
+    }
+
+    public function post() : BelongsTo
+    {
+        // Define the relationship with the Post model
+        return $this->belongsTo(Post::class);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['user_id' , 'title' , 'content'];
+
+    public function user() : BelongsTo
+    {
+        // Define the relationship with the User model
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments() : HasMany
+    {
+        // Define the relationship with the Comment model
+        return $this->hasMany(Comment::class);
+    }
 }
